2055b3a3ecbac6d2c05ff94e9191e014271f3401
TakanashiHiroto <213113@st.takushoku-u.ac.jp>
2023-12-11 12:05:46 +0900

    second commit

--- database/migrations/2023_12_11_013156_create_reviews_table.php
+++ database/migrations/2023_12_11_013156_create_reviews_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('sauna_background');
-            $table->rememberToken();
+            $table->string('body',500);
+            $table->integer('star');
+            $table->date('visited_at');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('sauna_id')->constrained();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 };
---
+++ resources/views/reviews/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>sauna</h1>
+        <div class='reviews'>
+            <div class='review'>
+                <h2 class='title'>レビューする</h2>
+                <p class='body'>いいサウナ</p>
+            </div>
+        </div>
+    </body>
+</html>
